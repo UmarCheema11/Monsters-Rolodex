0265eeeb5fc8a4c29578a5c022c9f61e6be166ff
Umar Ali Cheeama <umaralicheema9@gmail.com>
2020-07-22 20:00:29 +0500

    Updates

---
+++ precache-manifest.131fca3a13c5183ffa2599bff31d4d52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60075f23f8af95c203751f3e6845b75",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "91d6a87ee6986c3c426d",
+    "url": "/Monsters-Rolodex/static/css/main.3a33985f.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/Monsters-Rolodex/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91d6a87ee6986c3c426d",
+    "url": "/Monsters-Rolodex/static/js/main.03e2a335.chunk.js"
+  },
+  {
+    "revision": "321d78d19afe0ae88472",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.564099f5.js"
+  }
+]);
